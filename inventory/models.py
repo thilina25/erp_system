adccb699d146b16420d8763252aec862ef224c2f
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-01-19 00:17:18 +0530

    added model of inventory

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Supplier(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.models.CharField(max_length=15)
+    address = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    recoder_level = models.IntegerField()
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
