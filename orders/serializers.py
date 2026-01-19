758fc3cad81b302b6f81ce52e7a90ee109b15fdb
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-01-19 10:32:17 +0530

    Orders Serializer

---
+++ orders/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Order, OrderItem, Invoice
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Order
+        fields = '__all__'
+        
+class InvoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Invoice
+        fields = '__all__'
