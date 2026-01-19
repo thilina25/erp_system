88a1662bbba6690c61c301ecfc6e7f65771ce0df
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-01-19 09:20:31 +0530

    Orders Model added

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from inventory.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    STATUS_CHOICES = (
+        ('PENDING', 'Pending'),
+        ('APPROVED', 'Approved'),
+        ('SHIPPED', 'Shipped'),
+        ('COMPLETED', 'Completed'),
+        ('CANCELLED', 'Cancelled'),
+    )
+    
+    customer_name = models.CharField(max_length=150)
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    
+    def __Str__(self):
+        return f"Order #{self.id} - {self.customer_name}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity}"
+
+class Invoice(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    invoice_date = models.DateTimeField(auto_now_add=True)
+    pdf_url = models.CharField(max_length=225, blank=True, null=True)
+    
+    def __str__(self):
+        return f"Invoice for Order #{self.order.id}"
