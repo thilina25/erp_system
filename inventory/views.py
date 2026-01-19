5e9493144c9c1ab39b0c21ebc1b0bdc3ea5aed10
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-01-19 10:46:28 +0530

    inventory views added

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Product, Category, Supplier
+from .serializers import ProductSerializer, CategorySerializer, SupplierSerializer
+from users.permissions import IsAdmin, IsManager
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+    
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+    
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+    permission_classes = [IsAuthenticated]
