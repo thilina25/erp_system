dcb51767f0ddff26a62b2dd7b2f4bf1eae866de2
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-01-19 10:52:52 +0530

    Orders views added

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Order, OrderItem, Invoice
+from .serializers import OrderSerializer, OrderItemSerializer, InvoiceSerializer
 
-# Create your views here.
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+    
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+    permission_classes = [IsAuthenticated]
+    
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+    permission_classes = [IsAuthenticated]
