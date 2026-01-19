a9fa1eceea969953304974a6da2ea5d144d79fcf
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-01-19 13:04:30 +0530

    Auto Stock Deduction When Order Is Created

--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,62 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from django.db import transaction
+from inventory.models import Product
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from .models import Order, OrderItem, Invoice
 from .serializers import OrderSerializer, OrderItemSerializer, InvoiceSerializer
+
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
     
+    @transaction.atomic
+    def create(self, request, *args, **kwargs):
+        date = request.data
+        items = data.get("items")
+        
+        order = Order.objects.create(
+            customer_name=data.get("customer_name"),
+            status="PENDING",
+            total_amount=0
+        )
+        
+        total = 0
+        
+        for item in items:
+            product = Product.objects.get(id=item["product"])
+            qty = item ["quantity"]
+            
+            #Stock Check
+            if product.quantity < qty:
+                return Response(
+                    {"error": f"Not enough stock for {product.name}"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+                
+            #Deduct Stock
+            product.quantity -= qty
+            product.save()
+            
+            price = product.price * qty
+            total += price
+            
+            OrderItem.objecs.create(
+                order=order,
+                product=product,
+                quantity=qty,
+                price=product.price
+            )
+            
+            order.total_amount = total
+            order.save()
+            
+            serializer = self.get_serializer(order)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 class OrderItemViewSet(viewsets.ModelViewSet):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
